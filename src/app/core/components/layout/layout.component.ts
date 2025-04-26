2601863424d1c66a69bac3908013cef121978bd5
Scott R. Sonnenwald <ssonnenwald@gmail.com>
2025-04-26 01:00:34 -0400

    Cleaned up more imports

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -9,43 +9,28 @@ import {
   viewChild,
   WritableSignal,
 } from '@angular/core';
-import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import {
   Router,
   RouterLink,
   RouterLinkActive,
   RouterOutlet,
 } from '@angular/router';
-import {
-  MatSidenav,
-  MatSidenavContainer,
-  MatSidenavContent,
-} from '@angular/material/sidenav';
-import { MatIconButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import {
-  MatListItem,
-  MatListItemIcon,
-  MatListItemTitle,
-  MatNavList,
-} from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-layout',
   imports: [
     RouterOutlet,
-    MatToolbar,
-    MatToolbarRow,
+    MatToolbarModule,
     NgOptimizedImage,
-    MatSidenav,
-    MatSidenavContainer,
-    MatSidenavContent,
-    MatNavList,
-    MatListItem,
-    MatIcon,
-    MatListItemIcon,
-    MatListItemTitle,
-    MatIconButton,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
     RouterLink,
     RouterLinkActive,
   ],
